24f740ad765c886637aa783d89b6ae93a2db2972
aaryadevchandra <aaryadevc@gmail.com>
2025-06-22 20:10:21 +0530

    added: nv21 support

--- lib/main.dart
+++ lib/main.dart
@@ -176,20 +176,22 @@ class _PhyxPoCState extends State<PhyxPoC> {
       );
     }
 
-    // since format is constraint to nv21 or bgra8888, both only have one plane
-    if (image.planes.length != 1) return null;
-    final plane = image.planes.first;
+    if (Platform.isAndroid && format == InputImageFormat.nv21) {
+      // since format is constraint to nv21 or bgra8888, both only have one plane
+      if (image.planes.length != 1) return null;
+      final plane = image.planes.first;
 
-    // compose InputImage using bytes
-    return InputImage.fromBytes(
-      bytes: plane.bytes,
-      metadata: InputImageMetadata(
-        size: Size(image.width.toDouble(), image.height.toDouble()),
-        rotation: rotation, // used only in Android
-        format: format, // used only in iOS
-        bytesPerRow: plane.bytesPerRow, // used only in iOS
-      ),
-    );
+      // compose InputImage using bytes
+      return InputImage.fromBytes(
+        bytes: plane.bytes,
+        metadata: InputImageMetadata(
+          size: Size(image.width.toDouble(), image.height.toDouble()),
+          rotation: rotation, // used only in Android
+          format: format, // used only in iOS
+          bytesPerRow: plane.bytesPerRow, // used only in iOS
+        ),
+      );
+    }
   }
 
   void _startProcessing() {
